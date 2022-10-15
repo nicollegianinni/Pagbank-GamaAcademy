a126381c3498c6145ca2da47c37269d26c3585b9
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2022-10-15 12:20:55 -0300

    exercicios-aula-java

---
+++ java/src/atv1Java/operadorTernario.java
@@ -0,0 +1,18 @@
+package atv1Java;
+
+public class operadorTernario {
+
+	/* 
+	 reescrever o exercicio 1 fi-else com o operador ternario
+	 escrever uma classe, com um atributo int chamado salario.
+	 criar um metodo chamado informarValorImposto.
+	 caso salario seja maior que 5000 retornar 15. 
+	 caso nao retornar 5.
+	 */
+	
+	public static void main(String[] args) {
+		
+              em construção, ja subo!
+	}
+
+}
---
+++ java/src/atv1Java/salario.java
@@ -0,0 +1,27 @@
+package atv1Java;
+
+
+public class salario {
+
+/*exercicio 1 IF-ELSE: escreva uma classe, com um atributo int chamado salario. 
+Crie um metodo chamado informaValorImposto. e caso o salario seja maior que 5000, retorne 15. 
+Caso contrario, retorne 5. */
+
+	//metodo
+	void informaValorImposto(int salario) {
+
+		if (salario > 5000) {
+			System.out.println("\n 15");
+
+		} else {
+			System.out.println("\n 5 ");
+		}
+	}
+
+	//classe main
+	public static void main(String[] args) {
+
+		salario Salario = new salario();
+		Salario.informaValorImposto(6000); // (inclua o valor aqui para testar o metodo)
+	}
+}
